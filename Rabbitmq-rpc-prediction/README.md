f85d6ead8e7d8090ce36669afcea2a14e7c21336
Ertuğrul BAL <92356291+Ertugrulbal@users.noreply.github.com>
2022-04-05 00:55:24 +0300

    Add files via upload

---
+++ Rabbitmq-rpc-prediction/README.md
@@ -0,0 +1,30 @@
+cd # The Number Prediction Game with Golang and RabbitMQ
+
+## Dependencies used in the project
+
+- Go 1.17.3
+- RabbitMQ 3.9.14
+- Docker 20.10.4
+
+## Project Setup
+- Clone the project with `git clone https://github.com/Ertugrulbal/Number_Prediction_Game_With_Golang_RabbitMQ.git`
+## Run 
+- Run the project with `docker-compose up`
+
+## What is this project aims to?
+Picture of Project 
+* Program A generates a number between 0 and 9 and adds it to the NuerStoreA queue.
+* Program B also expects data from the NumberScoreB queue
+* If data comes from the queue, it makes 5 number predictions and if one of the prediction is correct, it gets 1 point to PointStore queue.
+* This process will continue continuously for both programs.
+
+
+
+
+
+## The Structure of Code 
+
+    -image of structure
+* Each of the Program have a Docker Image
+* There is a docker-compose.yml for the whole docker image work together. 
+* ProgramA and ProgramB have a Publisher Function and Consumer Function. 
---
+++ Rabbitmq-rpc-prediction/programB/programB.go
@@ -0,0 +1,267 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func FailOnError(err error, msg string) {
+	if err != nil {
+		log.Panicf("%s: %s", msg, err)
+	}
+}
+
+func RandomString(l int) string {
+	bytes := make([]byte, l)
+	for i := 0; i < l; i++ {
+		bytes[i] = byte(RandInt(65, 90))
+	}
+	return string(bytes)
+}
+
+func RandInt(min int, max int) int {
+	return min + rand.Intn(max-min)
+}
+func publishing(n int) (res int, err error) {
+	// Connection
+	conn, err := amqp.Dial("amqp://guest:guest@rabbitmq:5672/")
+	FailOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	//Channel Connection
+	ch, err := conn.Channel()
+	FailOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	//Exchange Declaration
+	err = ch.ExchangeDeclare(
+		"NumberStoreB", // name
+		"direct",       // type
+		true,           // durable
+		false,          // auto-deleted
+		false,          // internal
+		false,          // no-wait
+		nil,            // arguments
+	)
+	FailOnError(err, "Failed to declare an exchange")
+
+	// Queue Declaration
+	q, err := ch.QueueDeclare(
+		"",    // name
+		false, // durable
+		false, // delete when unused
+		true,  // exclusive
+		false, // noWait
+		nil,   // arguments
+	)
+	FailOnError(err, "Failed to declare a queue")
+
+	//Consume for Queue
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		true,   // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+	FailOnError(err, "Failed to register a consumer")
+
+	//CorrId Creation
+	corrId := RandomString(32)
+
+	//Publishing a data and declaration of queue
+	err = ch.Publish(
+		"NumberStoreB", // exchange
+		"rpc_queue2",   // routing key
+		false,          // mandatory
+		false,          // immediate
+		amqp.Publishing{
+			ContentType:   "text/plain",
+			CorrelationId: corrId,
+			ReplyTo:       q.Name,
+			Body:          []byte(strconv.Itoa(n)),
+		})
+	FailOnError(err, "Failed to publish a message")
+
+	for d := range msgs {
+		if corrId == d.CorrelationId {
+			res, err = strconv.Atoi(string(d.Body))
+			FailOnError(err, "Failed to convert body to integer")
+			break
+		}
+	}
+
+	return
+}
+
+/*
+Prediction function
+-If comes a produced number which from queue,
+-Consumer function working for the prediction.
+*/
+
+func prediction(n int) bool {
+
+	for i := 0; i < 5; i++ {
+
+		x := RandInt(0, 9)
+
+		if n == x {
+			fmt.Println("The random number is predicted! Congrulations!")
+			return true
+		}
+
+	}
+	return false
+}
+
+/*
+	Publisher Function. The Work pipline is:
+ 1-) Create a random number,
+ 2-) The number delivered to Consumer of Program B.
+ 3-) Consumer of Program B received a number and return score B.
+ 4*) At the end of function, the result of ScoreB print at the screen.
+
+*/
+func publisher() {
+	// Thanks to this for loop, the machine deliver a random number to the consumer every 3 seconds.
+	for {
+		rand.Seed(time.Now().UTC().UnixNano())
+
+		n := RandInt(0, 9)
+
+		log.Printf(" [x] The Random number from ProgramA is: (%d)", n)
+		res, err := publishing(n)
+		FailOnError(err, "Failed to handle RPC request")
+
+		log.Printf(" [.] Point Store B is: %d", res)
+		time.Sleep(3 * time.Second)
+	}
+}
+
+/*
+	Consumer Function. The Work pipline is:
+ 1-) Read a queue,
+ 2-) predict a random number
+ 3-) If Prediction is achieved, Score value increment 1, Otherwise break a loop
+
+*/
+func consumer() {
+	conn, err := amqp.Dial("amqp://guest:guest@rabbitmq:5672/")
+	FailOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	FailOnError(err, "Failed to open a channel")
+	defer ch.Close()
+	err = ch.ExchangeDeclare(
+		"NumberStoreA", // name
+		"direct",       // typea
+		true,           // durable
+		false,          // auto-deleted
+		false,          // internal
+		false,          // no-wait
+		nil,            // arguments
+	)
+	FailOnError(err, "Failed to declare an exchange")
+	q, err := ch.QueueDeclare(
+		"rpc_queue", // name
+		false,       // durable
+		false,       // delete when unused
+		false,       // exclusive
+		false,       // no-wait
+		nil,         // arguments
+	)
+	FailOnError(err, "Failed to declare a queue")
+
+	// The value defines the max number of unacknowledged deliveries that are permitted on a channel.
+	err = ch.Qos(
+		1,     // prefetch count
+		0,     // prefetch size
+		false, // global
+	)
+	FailOnError(err, "Failed to set QoS")
+
+	// The function provide a binding with some specifications
+	err = ch.QueueBind(
+		q.Name,         // queue name
+		"rpc_queue",    // routing key
+		"NumberStoreA", // exchange
+		false,
+		nil,
+	)
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		false,  // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+	FailOnError(err, "Failed to register a consumer")
+
+	var forever chan struct{}
+	var scoreB int
+	go func() {
+		for d := range msgs {
+			n, err := strconv.Atoi(string(d.Body))
+			FailOnError(err, "Failed to convert body to integer")
+
+			log.Printf(" [.] Prediction(%d)", n)
+			response := prediction(n)
+
+			if response {
+				scoreB += 1
+
+			} else {
+
+			}
+
+			err = ch.Publish(
+				"",        // exchange
+				d.ReplyTo, // routing key
+				false,     // mandatory
+				false,     // immediate
+				amqp.Publishing{
+					ContentType:   "text/plain",
+					CorrelationId: d.CorrelationId,
+					Body:          []byte(strconv.Itoa(scoreB)),
+				})
+			FailOnError(err, "Failed to publish a message")
+
+			d.Ack(false)
+		}
+	}()
+
+	log.Printf(" [*] Awaiting RPC requests")
+	<-forever
+}
+func main() {
+	//Main part of the Program A. We use a Go Routine function for the Consumer.
+	go consumer()
+	time.Sleep(3 * time.Second)
+	publisher()
+
+}
+
+func bodyFrom(args []string) int {
+	var s string
+	if (len(args) < 2) || os.Args[1] == "" {
+		s = "30"
+	} else {
+		s = strings.Join(args[1:], " ")
+	}
+	n, err := strconv.Atoi(s)
+	FailOnError(err, "Failed to convert arg to integer")
+	return n
+}
